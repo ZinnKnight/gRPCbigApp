80106261b0903fb49e86c8dd869884db80648bbf
Mark Varisov <mark.varisov.go@gmail.com>
2026-05-27 21:27:50 +0300

    redis fix

--- Shared/Redis/RedisClient.go
+++ Shared/Redis/RedisClient.go
@@ -7,21 +7,37 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+type Config struct {
+	Addr         string
+	Password     string
+	DB           int
+	PoolSize     int
+	MinIdleConns int
+}
+
 type RedisClient struct {
 	*redis.Client
 }
 
-func NewRedisClient(ctx context.Context, addres, password string, redisDB int) (*RedisClient, error) {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     addres,
-		Password: password,
-		DB:       redisDB,
-	})
+func NewRedisClient(ctx context.Context, config Config) (*RedisClient, error) {
+	options := &redis.Options{
+		Addr:     config.Addr,
+		Password: config.Password,
+		DB:       config.DB,
+	}
+	if config.PoolSize > 0 {
+		options.PoolSize = config.PoolSize
+	}
+	if config.MinIdleConns > 0 {
+		options.MinIdleConns = config.MinIdleConns
+	}
 
+	rdb := redis.NewClient(options)
 	if err := rdb.Ping(ctx).Err(); err != nil {
-		return nil, fmt.Errorf("redis, ping was failed: %w", err)
+		return nil, fmt.Errorf("failed to ping to redis: %w", err)
 	}
 	return &RedisClient{Client: rdb}, nil
+
 }
 
 func (rc *RedisClient) Close() error {
