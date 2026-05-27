b6e4f126e486f795f81b490fef772ba8e8b51e82
Mark Varisov <mark.varisov.go@gmail.com>
2026-05-27 18:51:29 +0300

    Metrics fixed

--- Shared/Metrics/Metrics.go
+++ Shared/Metrics/Metrics.go
@@ -1,13 +1,42 @@
 package Metrics
 
 import (
+	"gRPCbigapp/Shared/Metrics/MetricsPort"
+	"net/http"
+
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var GRPCRequestTotal = promauto.NewCounterVec(prometheus.CounterOpts{
-	Name: "grpc_requests_total",
-	Help: "Total grpc request by method and status code",
-},
-	[]string{"method", "status_code"},
-)
+var _ MetricsPort.MetricsRecord = (*PrometheusRecord)(nil)
+
+type PrometheusRecord struct {
+	registry *prometheus.Registry
+	reqTotal *prometheus.CounterVec
+}
+
+func NewPrometheusRecord() *PrometheusRecord {
+	reg := prometheus.NewRegistry()
+
+	reg.MustRegister(collectors.NewGoCollector(), collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
+
+	reqTotal := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "grpc_request_total",
+		Help: "Total grpc request, including status codes and methods",
+	}, []string{"method", "status_codes"})
+	reg.MustRegister(reqTotal)
+
+	return &PrometheusRecord{
+		registry: reg,
+		reqTotal: reqTotal,
+	}
+}
+
+func (prom *PrometheusRecord) IncRequest(method, statusCode string) {
+	prom.reqTotal.WithLabelValues(method, statusCode).Inc()
+}
+
+func (prom *PrometheusRecord) Registry() http.Handler {
+	return promhttp.HandlerFor(prom.registry, promhttp.HandlerOpts{})
+}
